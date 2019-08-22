a95f1fbcf4a54a98a85a399fabe702e039e083fb
Melanie <mel.lontsi@gmail.com>
2019-08-22 16:17:20 -0700

    added app main activities

---
+++ app/src/main/java/com/mkenlo/lingua/UserRecordings.java
@@ -0,0 +1,13 @@
+package com.mkenlo.lingua;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserRecordings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_recording);
+    }
+}
